f8b47dfe111ce7c0bae23dd9d3b4dc8002c8b899
Andrew Hale <andrew.hale003@gmail.com>
2023-03-01 21:04:01 +1000

    const change

--- src/RomanNumerals.jl
+++ src/RomanNumerals.jl
@@ -2,17 +2,17 @@ module RomanNumerals
 
 export to_int, to_roman_numeral
 
-function build_regex(roman_numerals)
+function build_regex(roman_numeral)
     regex = string()
-    for (sym, _) ∈ roman_numerals
+    for (sym, _) ∈ roman_numeral
         regex *= "($sym)*"
     end
     return Regex(regex)
 end
 
-const roman_numerals = [("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90), 
+const ROMAN_NUMERALS = [("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90), 
                         ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)]
-const roman_numeral_regex = build_regex(roman_numerals)
+const ROMAN_NUMERALS_REGEX = build_regex(ROMAN_NUMERALS)
 
 to_roman_numeral(n::Any) = throw(ArgumentError("Expected an Int, got $n"))
 function to_roman_numeral(n::Int)
@@ -21,7 +21,7 @@ function to_roman_numeral(n::Int)
     end
 
     roman_numeral = string()
-    for (sym, value) ∈ roman_numerals
+    for (sym, value) ∈ ROMAN_NUMERALS
         sym_fit = div(n, value)
         roman_numeral *= repeat(sym, sym_fit)
         n -= value * sym_fit
@@ -31,15 +31,15 @@ end
 
 to_int(rn::Any) = throw(ArgumentError("Expected a String, got $rn"))
 function to_int(rn::String)
-    if (match(roman_numeral_regex, rn).match == "")
+    if (match(ROMAN_NUMERALS_REGEX, rn).match == "")
         throw(ArgumentError("Expected a valid Roman Numeral, got $rn"))
     end
 
     n = 0
-    for (sym, value) ∈ roman_numerals
+    for (sym, value) ∈ ROMAN_NUMERALS
         while first(rn, length(sym)) == sym
             n += value
-            rn = rn[(length(sym)+1):end] # peel first symbol
+            rn = rn[(length(sym)+1):end] # remove leftmost symbol
         end
     end
     return n
