b7679a1821b23402d8cbecc7f717f0179dae232b
Andrew Hale <andrew.hale003@gmail.com>
2023-03-01 22:05:39 +1000

    cleaned

--- src/RomanNumerals.jl
+++ src/RomanNumerals.jl
@@ -2,17 +2,9 @@ module RomanNumerals
 
 export to_int, to_roman_numeral
 
-function build_regex(roman_numeral)
-    regex = string()
-    for (sym, _) ∈ roman_numeral
-        regex *= "($sym)*"
-    end
-    return Regex(regex)
-end
-
 const ROMAN_NUMERALS = [("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100), ("XC", 90), 
                         ("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I", 1)]
-const ROMAN_NUMERALS_REGEX = build_regex(ROMAN_NUMERALS)
+const ROMAN_NUMERALS_REGEX = Regex(mapreduce(x -> "(" * x[1] * ")*", *, ROMAN_NUMERALS))
 
 to_roman_numeral(n::Any) = throw(ArgumentError("Expected an Int, got $n"))
 function to_roman_numeral(n::Int)
